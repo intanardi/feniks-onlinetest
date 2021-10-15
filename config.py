5369bccb95b88b0a404f27ed6b16258978f39ef5
Ardi <intan.ardipramanova@gmail.com>
2021-10-15 13:57:14 +0700

    new config

--- config.py
+++ config.py
@@ -6,6 +6,7 @@ class Config:
     MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.googlemail.com')
     MAIL_PORT = int(os.environ.get('MAIL_PORT', '587'))
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'true').lower() in ['true', 'on', '1']
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     FLASKY_MAIL_SUBJECT_PREFIX = '[socialblog]'
@@ -24,18 +25,18 @@ class Config:
 class DevelopmentConfig(Config):
     DEBUG = True
     # SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URI') or 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:@localhost/feniks_online_test'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@localhost/feniks_online_test'
     # 'SQLALCHEMY_TRACK_MODIFICATIONS' = Falsedeactivate
     # SQLALCHEMY_DATABASE_URI = 'mysql://root:dsai99asdu8x@9@localhost/orb_metropolis'
 
 class TestingConfig(Config):
     TESTING = True
     # SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URI') or 'sqlite://'
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:@localhost/feniks_online_test'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@localhost/feniks_online_test'
 
 class ProductionConfig(Config):
     # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:@localhost/feniks_online_test'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@localhost/feniks_online_test'
 
 config = {
     'development' : DevelopmentConfig,
